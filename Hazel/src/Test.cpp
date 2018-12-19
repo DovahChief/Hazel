0413c1480bf3dc159f491a598b9cbeebec675153
Jose Antonio <pepelimonta@gmail.com>
2018-12-19 12:00:14 -0600

    Commit inicial de setup

---
+++ Hazel/src/Test.cpp
@@ -0,0 +1,19 @@
+#include "Test.h"
+#include <iostream>
+
+namespace Hazel {
+
+	void Print() {
+		std::cout << "Bienvenido a Hazel";
+	}
+
+	Test::Test()
+	{
+	}
+
+
+	Test::~Test()
+	{
+	}
+
+}
---
+++ Hazel/src/Test.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+namespace Hazel {
+
+	_declspec(dllexport) void Print();
+
+	class Test
+	{
+	public:
+		Test();
+		~Test();
+	};
+}
+
+
